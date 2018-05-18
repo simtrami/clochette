0cd9b585c67d8919c591ba17ce5efd853caa6729
teagrain <pierrick.gorisse@gmail.com>
2018-05-18 04:11:21 +0200

    avancé sur controller et vue purchase, faut encore s'occuper du panier du client, je vois pas trop comment faire mais j'essairais

--- clochette/src/AppBundle/Controller/PurchaseController.php
+++ clochette/src/AppBundle/Controller/PurchaseController.php
@@ -17,8 +17,41 @@ class PurchaseController extends Controller
         if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
             throw $this->createAccessDeniedException();
         }
-        
-        return $this->render('purchase/index.html.twig');
+
+        $em = $this->getDoctrine()->getManager();
+        $repo_stocks = $this->getDoctrine()->getRepository('AppBundle:Stocks');
+        $conn = $this->getDoctrine()->getManager()->getConnection();
+
+
+        $bottles_notnull = $repo_stocks->findBy(
+            array('type' => 'bottle', 'quantite' => 15)
+        );
+
+        $article_notnull = $repo_stocks->findBy(
+            array('type' => 'article', 'quantite' => 25)
+        );
+
+        /* futs */ $sql = ' SELECT * FROM stocks S WHERE S.type="draft" AND S.quantite > :quantite ';
+
+        $drafts_notnull = $conn->prepare($sql);
+        $drafts_notnull->execute(['quantite' => 0]);
+
+        /* bouteilles */ $sql = ' SELECT * FROM stocks S WHERE S.type="bottle" AND S.quantite > :quantite ';
+
+        $bottles_notnull = $conn->prepare($sql);
+        $bottles_notnull->execute(['quantite' => 0]);
+
+        /* articles */ $sql = ' SELECT * FROM stocks S WHERE S.type="article" AND S.quantite > :quantite ';
+
+        $article_notnull = $conn->prepare($sql);
+        $article_notnull->execute(['quantite' => 0]);
+
+        $data=[];
+        $data['drafts_notnull'] = $drafts_notnull;
+        $data['bottles_notnull'] = $bottles_notnull;
+        $data['article_notnull'] = $article_notnull;
+
+        return $this->render("purchase/index.html.twig", $data);
                                 
     }
 }
