c4eab31923639c067fb478270c66ff9886d8fd78
Simtrami <clement.taverne@telecom-sudparis.eu>
2018-05-24 23:48:42 +0200

    Voilà les 5 jours de travail... Il reste des améliorations de toute évidence. Il faut mettre les forms dans une collection sinon il faut un submit par ligne.

--- clochette/src/AppBundle/Controller/PreparationController.php
+++ clochette/src/AppBundle/Controller/PreparationController.php
@@ -2,6 +2,7 @@
 // src/AppBundle/Controller/PreparationController.php
 namespace AppBundle\Controller;
 
+use AppBundle\Form\SelectArticleType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,24 +11,15 @@ use AppBundle\Entity\Stocks;
 
 class PreparationController extends Controller
 {
-    
-    /**
-     * @Route("/preparation", name="preparation")
-     **/
-    public function showIndex()
-    {
-        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
-            throw $this->createAccessDeniedException();
-        }
-        
-        return $this->render('preparation/preparation.html.twig');
-                                
-    }
 
     /**
     * @Route("/preparation", name="preparation")
     **/
     public function getData(Request $request){
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw $this->createAccessDeniedException();
+        }
+
         $em = $this->getDoctrine()->getManager();
         $repo_stocks = $this->getDoctrine()->getRepository('AppBundle:Stocks');
         $conn = $this->getDoctrine()->getManager()->getConnection();
@@ -44,18 +36,111 @@ class PreparationController extends Controller
 
         /* articles */ $sql = ' SELECT * FROM stocks S WHERE S.type="article" AND S.quantite > :quantite ';
         
-        $article_notnull = $conn->prepare($sql);
-        $article_notnull->execute(['quantite' => 0]);
+        $articles_notnull = $conn->prepare($sql);
+        $articles_notnull->execute(['quantite' => 0]);
 
         $data=[];
         $data['drafts_notnull'] = $drafts_notnull;
         $data['bottles_notnull'] = $bottles_notnull;
-        $data['article_notnull'] = $article_notnull;
+        $data['articles_notnull'] = $articles_notnull;
 
-        $idForSale=$request->request->get(articlesForSale);
-        foreach ($idForSale as $cur_id){
-            $repo_stocks->findOneBy(array('idarticle' => $cur_id))->setIsForSale(1);
+        /* $stock = $repo_stocks->findAll();
+
+        foreach ($stock as $article){
+            $uniqueFormName = $article->getIdarticle(); //Use some unique data to generate the form name.
+            $form = $this->get('form.factory')->createNamed($uniqueFormName, SelectArticleType::class, $article);
+            $stockForms[$uniqueFormName] = $form;
         }
+        foreach ($stockForms as $formName => $form){
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $article = $repo_stocks->findByIdarticle($formName);
+                $em->persist($article);
+                $em->flush();
+                
+            }
+            $data['stockForms'][$formName] = $form->createView();
+        }
+ */
+
+        $drafts = $repo_stocks->findByType("draft");
+        $bottles = $repo_stocks->findByType("bottle");
+        $articles = $repo_stocks->findByType("article");
+
+        foreach ($drafts as $draft){
+            $uniqueFormName = $draft->getIdarticle(); //Use some unique data to generate the form name.
+            $form = $this->get('form.factory')->createNamed($uniqueFormName, SelectArticleType::class, $draft);
+            $draftForms[$uniqueFormName] = $form;
+        }
+        foreach ($bottles as $bottle){
+            $uniqueFormName = $bottle->getIdarticle(); //Use some unique data to generate the form name.
+            $form = $this->get('form.factory')->createNamed($uniqueFormName, SelectArticleType::class, $bottle);
+            $bottleForms[$uniqueFormName] = $form;
+        }
+        foreach ($articles as $article){
+            $uniqueFormName = $article->getIdarticle(); //Use some unique data to generate the form name.
+            $form = $this->get('form.factory')->createNamed($uniqueFormName, SelectArticleType::class, $article);
+            $articleForms[$uniqueFormName] = $form;
+        }
+
+        foreach ($draftForms as $formName => $form){
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $draft = $repo_stocks->find($form->getName());
+                $em->persist($draft);
+                $em->flush();
+                return $this->redirectToRoute('preparation');
+            }
+            $data['draftForms'][$formName] = $form->createView();
+        }
+        foreach ($bottleForms as $formName => $form){
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $bottle = $repo_stocks->find($formName);
+                $em->persist($bottle);
+                $em->flush();
+                return $this->redirectToRoute('preparation');
+            }
+            $data['bottleForms'][$formName] = $form->createView();
+        }
+        foreach ($articleForms as $formName => $form){
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $article = $repo_stocks->find($formName);
+                $em->persist($article);
+                $em->flush();
+                return $this->redirectToRoute('preparation');
+            }
+            $data['articleForms'][$formName] = $form->createView();
+        }
+
+        /* if (!is_null($request->query->get("articlesForSale"))){
+            $idArticlesForSale = json_decode($request->query->get("articlesForSale"));
+            if (!is_null($idArticlesForSale)) {
+                foreach ($idArticlesForSale as $id) {
+                    $article = $em->getRepository('AppBundle:Stocks')->find($id);
+                    if (is_null($article)) {
+                        throw $this->createNotFoundException("Article non trouvé pour l'id ".$idarticle);
+                    } else {
+                        $article->setIsForSale(1);
+                    }
+                }
+            }
+
+        } */
+
+        /* $idList = $request->query->get('articlesForSale');
+        if (!is_null($idList)){
+            $idForSale = explode(",", $idList, -1);
+            foreach ($idForSale as $id){
+                $article = $em->getRepository('AppBundle:Stocks')->find($id);
+                if (is_null($article)) {
+                    throw $this->createNotFoundException("Article non trouvé pour l'id ".$idarticle);
+                } else {
+                    $article->setIsForSale(1);
+                }
+            }   
+        } */
 
         return $this->render("preparation/preparation.html.twig", $data);
     }
--- clochette/src/AppBundle/Controller/PurchaseController.php
+++ clochette/src/AppBundle/Controller/PurchaseController.php
@@ -23,17 +23,17 @@ class PurchaseController extends Controller
         $conn = $this->getDoctrine()->getManager()->getConnection();
 
 
-        /* futs */ $sql = ' SELECT * FROM stocks S WHERE S.type="draft" AND S.is_forSale = :x';
+        /* futs */ $sql = ' SELECT * FROM stocks S WHERE S.type="draft" AND S.isForSale = :x';
 
         $selected_drafts = $conn->prepare($sql);
         $selected_drafts -> execute(['x' => 1]);
 
-        /* bouteilles */ $sql = ' SELECT * FROM stocks S WHERE S.type="bottle" AND S.is_forSale = :x';
+        /* bouteilles */ $sql = ' SELECT * FROM stocks S WHERE S.type="bottle" AND S.isForSale = :x';
 
         $selected_bottles = $conn->prepare($sql);
         $selected_bottles -> execute(['x' => 1]);
 
-        /* articles */ $sql = ' SELECT * FROM stocks S WHERE S.type="article" AND S.is_forSale = :x';
+        /* articles */ $sql = ' SELECT * FROM stocks S WHERE S.type="article" AND S.isForSale = :x';
 
         $selected_articles = $conn->prepare($sql);
         $selected_articles -> execute(['x' => 1]);
