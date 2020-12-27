ba3b5fa93c7fbb18b7a4c9dfce4d78288f5439b0
Clément T <clement.taverne@telecom-sudparis.eu>
2020-12-27 02:42:21 +0100

    Finish importing dependencies
    Signed-off-by: Clément T <clement.taverne@telecom-sudparis.eu>

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Algolia\SearchBundle\AlgoliaSearchBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
