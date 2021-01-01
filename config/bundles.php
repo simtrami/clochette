d89a96ec7fff6ec3d45434be45ebb465efa0a6fa
Clément T <clement.taverne@telecom-sudparis.eu>
2021-01-01 22:40:14 +0100

    Add logging with Monolog
    Signed-off-by: Clément T <clement.taverne@telecom-sudparis.eu>

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Algolia\SearchBundle\AlgoliaSearchBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
