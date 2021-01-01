729ce4db27a6b47c92b2aae8e1497b680355500f
Clément T <clement.taverne@telecom-sudparis.eu>
2021-01-01 21:28:40 +0100

    Trust Clever Cloud proxies with the Symfony 4.4 method
    Signed-off-by: Clément T <clement.taverne@telecom-sudparis.eu>

--- public/index.php
+++ public/index.php
@@ -12,7 +12,15 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
+$request = Request::createFromGlobals();
+
+if ($trustedProxies = $request->server->get('CC_REVERSE_PROXY_IPS') ?? false) {
+    // trust *all* requests
+    Request::setTrustedProxies(array_merge(['127.0.0.1'], explode(',', $trustedProxies)),
+
+    // trust *all* "X-Forwarded-*" headers
+    Request::HEADER_X_FORWARDED_ALL);
+} elseif ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
 }
 
