f8b6b3379d66b478948a4f15c10c03886279b058
Clément T <clement.taverne@telecom-sudparis.eu>
2021-01-15 19:40:23 +0100

    Assets compilation on deployment
    Signed-off-by: Clément T <clement.taverne@telecom-sudparis.eu>

--- clevercloud/post_build.sh
+++ clevercloud/post_build.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
 # Database migrations
 ./bin/console doctrine:migrations:migrate -n
+# Frontend build
+yarn install --non-interactive --pure-lockfile --force --production=false && yarn run build
